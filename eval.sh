74e8da048ee9f3e65ffb8ea68eb53254a4dd684f
PriceWang <0428wgx@gmail.com>
2025-01-06 15:55:57 +0000

    with cinc2021

--- eval.sh
+++ eval.sh
@@ -2,7 +2,7 @@
 # Author: Guoxin Wang
 # Date: 2024-12-05 15:22:04
 # LastEditors: Guoxin Wang
-# LastEditTime: 2025-01-02 15:13:39
+# LastEditTime: 2025-01-06 15:40:22
 # FilePath: /DNSECG/eval.sh
 # Description:
 #
@@ -10,8 +10,7 @@
 ###
 
 python training.py \
-    --test_path ./datasets/mitdb/af_beat_4_test.pth \
-    --output_dir ./ckpts/lam0.4 \
+    --output_dir ./ckpts/lam0 \
     --eval # ./datasets/incartdb/af_beat_4_train.pth \
 # ./datasets/incartdb/af_beat_4_valid.pth \
 # ./datasets/incartdb/af_beat_4_test.pth \
--- train.sh
+++ train.sh
@@ -2,119 +2,119 @@
 # Author: Guoxin Wang
 # Date: 2024-11-29 12:58:55
 # LastEditors: Guoxin Wang
-# LastEditTime: 2024-12-20 11:42:21
-# FilePath: /workspace_3090/DNSECG/train.sh
+# LastEditTime: 2025-01-04 21:07:48
+# FilePath: /DNSECG/train.sh
 # Description:
 #
 # Copyright (c) 2024 by Guoxin Wang, All Rights Reserved.
 ###
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0 \
     --log_dir ./ckpts/lam0
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.1 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.1 \
     --log_dir ./ckpts/lam0.1
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.2 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.2 \
     --log_dir ./ckpts/lam0.2
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.3 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.3 \
     --log_dir ./ckpts/lam0.3
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.4 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.4 \
     --log_dir ./ckpts/lam0.4
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.5 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.5 \
     --log_dir ./ckpts/lam0.5
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.6 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.6 \
     --log_dir ./ckpts/lam0.6
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.7 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.7 \
     --log_dir ./ckpts/lam0.7
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.8 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.8 \
     --log_dir ./ckpts/lam0.8
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 0.9 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam0.9 \
     --log_dir ./ckpts/lam0.9
 
 OMP_NUM_THREADS=20 torchrun --nnodes=1 --nproc-per-node=1 training.py \
-    --batch_size 256 \
+    --batch_size 1024 \
     --lr 1e-3 \
-    --epochs 20 \
+    --epochs 50 \
     --tau 0.05 \
     --lam 1 \
-    --warmup_epochs 0 \
+    --warmup_epochs 5 \
     --output_dir ./ckpts/lam1 \
     --log_dir ./ckpts/lam1
